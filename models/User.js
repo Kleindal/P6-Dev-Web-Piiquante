a62a13072ee9937aaea467ce66fcc3c07aa4773b
Kleindal <kleindalsama@gmail.com>
2021-12-21 23:50:32 +0100

    Controllers and models user

--- models/User.js
+++ models/User.js
@@ -6,6 +6,6 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: true }
 });
 
-userSchema.plugin(uniqueValidator);
+userSchema.plugin(uniqueValidator, { message: 'This {PATH} is already use.' });
 
 module.exports = mongoose.model('User', userSchema);
